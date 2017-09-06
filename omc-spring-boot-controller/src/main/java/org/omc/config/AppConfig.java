b2b81e54c01c270d9b73272da79a49d73ed75fb8
T803465 <T803465@AK0275910.tokiomarine.com.br>
2017-09-06 13:51:39 -0300

    Criando intercpetors

---
+++ omc-spring-boot-controller/src/main/java/org/omc/config/AppConfig.java
@@ -0,0 +1,20 @@
+package org.omc.config;
+
+import org.omc.interceptor.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class AppConfig  extends WebMvcConfigurerAdapter {
+
+	
+	@Autowired RequestInterceptor requestInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry  registry) {
+		registry.addInterceptor(requestInterceptor);
+	}
+	
+}
