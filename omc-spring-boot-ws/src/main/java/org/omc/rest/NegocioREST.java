f18a28ddb48eb7fa8cde6513c1241cebdd5038e0
Ocean Home <oceancortez@gmail.com>
2017-09-07 20:08:22 -0300

    Inclusão do módulo WS

--- omc-spring-boot-ws/src/main/java/org/omc/rest/NegocioREST.java
+++ omc-spring-boot-ws/src/main/java/org/omc/rest/NegocioREST.java
@@ -4,8 +4,8 @@ import org.omc.seguro.service.NegocioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,13 +16,13 @@ public class NegocioREST {
 	
 	@Autowired NegocioService negocioService;
 	
-	@RequestMapping(value = "getNegocio", method = RequestMethod.GET)
+	@GetMapping(value = "getNegocio" )
 	public @ResponseBody ResponseEntity<?> getNegocio(){
 		
 		return new ResponseEntity<>(negocioService.getNegocio(), HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "getNegocios", method = RequestMethod.GET)
+	@GetMapping(value = "getNegocios" )
 	public @ResponseBody ResponseEntity<?> getNegocios(){
 		
 		return new ResponseEntity<>(negocioService.getNegocios(), HttpStatus.OK);
